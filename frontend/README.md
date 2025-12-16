39b2efaa47d4f80797fbe03e2197a16ae467e1cc
b01tech <bruno01.tech@gmail.com>
2025-12-16 08:45:52 -0300

    chore: initial project frontend setup

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+# Frontend Evento Digital
---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const font = Inter({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Seu Evento começa aqui",
+  description: "Aplicação fullstack de gerenciamento de eventos",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={`${font.className}`}>{children}</body>
+    </html>
+  );
+}
