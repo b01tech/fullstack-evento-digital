72ef979adfb3808e5f0bcaf76127d09d4b76b70f
b01tech <bruno01.tech@gmail.com>
2025-12-17 14:39:50 -0300

    feat: add DashboardEvent component

---
+++ frontend/src/components/event/DashboardEvent.tsx
@@ -0,0 +1,15 @@
+import { Event } from "@/src/core/models";
+import InfoEvent from "./InfoEvent";
+
+export interface DashboardEventProps {
+  event: Event;
+}
+
+export default function DashboardEvent(props: DashboardEventProps) {
+  const { event } = props;
+  return (
+    <div className="flex flex-col items-center justify-start min-h-screen">
+      <InfoEvent event={event} />
+    </div>
+  );
+}
