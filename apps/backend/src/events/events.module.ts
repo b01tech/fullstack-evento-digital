fb506ed964b392f83b2e3602f164ba4d60d0bc53
b01tech <bruno01.tech@gmail.com>
2025-12-18 17:05:43 -0300

    feat(events): add events controller to module

--- apps/backend/src/events/events.module.ts
+++ apps/backend/src/events/events.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DbModule } from 'src/db/db.module';
 import { EventsService } from './events.service';
+import { EventsController } from './events.controller';
 
 @Module({
   imports: [DbModule],
+  controllers: [EventsController],
   providers: [EventsService],
 })
 export class EventsModule {}
