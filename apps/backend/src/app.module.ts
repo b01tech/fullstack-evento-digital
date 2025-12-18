6bd5d812c2db303cd1edc80379d71773cbf1ac83
b01tech <bruno01.tech@gmail.com>
2025-12-18 14:06:36 -0300

    feat(db): add database module with Prisma provider

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { EventsModule } from './events/events.module';
 import { EventsController } from './events/events.controller';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [EventsModule],
+  imports: [EventsModule, DbModule],
   controllers: [AppController, EventsController],
   providers: [],
 })
---
+++ apps/backend/src/db/prisma.provider.ts
@@ -0,0 +1,15 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma';
+
+@Injectable()
+export class PrismaProvider
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
